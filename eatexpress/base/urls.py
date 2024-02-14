77b9837b23bae166b54480f72cc86b75137a9d21
Iseg6ix <freddyjseg@gmail.com>
2024-02-14 10:05:25 -0500

    Minor changes

--- eatexpress/base/urls.py
+++ eatexpress/base/urls.py
@@ -5,7 +5,7 @@ from users import views as user_views
 
 
 urlpatterns = [
-    path('home/', views.home, name = 'home'),
+    path('', views.home, name = 'home'),
     path('about/', views.about, name='about'),
     path('menu/', views.menu, name='menu'),
     path('api/dishes/', views.get_dishes, name='get_dishes'),
