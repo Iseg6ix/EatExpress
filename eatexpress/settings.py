cd719dd44129f7e7306ed5e594604ac65394184d
Iseg6ix <freddyjseg@gmail.com>
2024-07-27 19:43:00 -0400

    fix

--- eatexpress/settings.py
+++ eatexpress/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
     # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    }
 }
 
 # database_url = os.environ.get("DATABASE_URL")
